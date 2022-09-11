532e4391c4e83c7843a025ec2247b35a380d9ffb
dmitriy <dmitbely@protonmail.ch>
2022-09-11 14:06:52 +0300

    Seminar 08 added

---
+++ Seminars/Seminar08/task01/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+void PrintNumbers(int N)
+{
+    if(N < 1) return;
+    Console.Write(N + " ");
+    PrintNumbers(N - 1);
+}
+PrintNumbers(20);
---
+++ Seminars/Seminar08/task02/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+
+void PrintArray(int n)
+{
+    if(n < 1) return;
+    PrintArray(n - 1);
+    Console.Write(n + " ");
+}
+
+PrintArray(5);
